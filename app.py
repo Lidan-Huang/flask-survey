841d1e82146dab76943ddb7d0ab8aed45f59f5b4
Lidan-Huang <huangld0314@gmail.com>
2022-01-27 18:01:59 -0800

    finished showing questions and handling answers using sessions

--- app.py
+++ app.py
@@ -8,12 +8,71 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+RESPONSES_KEY = "responses"
+
 @app.get('/')
-def start_survey():
+def show_survey():
     """Shows the start survey page"""
+
+    session['responses'] = []
 
     return render_template(
         "survey_start.html",
         title=survey.title,
         instructions=survey.instructions
     )
+
+@app.get("/question/<int:question_number>")
+def show_question(question_number):
+    """Show page with the next question"""
+
+    if len(session[RESPONSES_KEY]) == len(survey.questions):
+        flash("You have answered all the questions")
+        return redirect("/completion")
+
+    if question_number >= len(survey.questions) or question_number > len(session[RESPONSES_KEY]):
+        question_number = len(session[RESPONSES_KEY])
+        flash("Stop trying to skip my questions")
+        return redirect(f"/question/{question_number}")
+
+    question_number = len(session[RESPONSES_KEY])
+
+    question = survey.questions[question_number]
+
+    return render_template(
+        "question.html",
+        question=question
+        # question_number = question_number + 1
+    )
+
+ 
+@app.post("/answer")
+def collect_answers():
+    """Adds selected answers to global response variable list
+    Redirects to next question, if there are no more questions,
+    redirects to thank you page.
+    """
+    answer = request.form["answer"]
+    responses = session[RESPONSES_KEY]
+    responses.append(answer)
+    session[RESPONSES_KEY] = responses
+
+    if len(responses) == len(survey.questions):
+        return redirect("/completion")
+    else:
+        # question_number = request.form["question_number"]
+        question_number = len(responses)
+        return redirect(f"/question/{question_number}")
+
+
+@app.get("/completion")
+def finish_survey():
+    """Shows thank you message for completing the survey"""
+
+    return render_template("completion.html")
+
+"""
+    can't directly append data to session[RESPONSES_KEY], as the list inside can't
+    update automatically, so we need to rebind to it, like line 56-58
+
+"""  
