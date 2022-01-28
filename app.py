d3c1ecbe664c072658002ddef3294474e315a238
Lidan-Huang <huangld0314@gmail.com>
2022-01-27 17:32:55 -0800

    finished show_survey page

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/')
+def start_survey():
+    """Shows the start survey page"""
+
+    return render_template(
+        "survey_start.html",
+        title=survey.title,
+        instructions=survey.instructions
+    )
